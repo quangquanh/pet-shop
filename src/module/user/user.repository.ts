10122d12530355e18a7b5a633cb0a813f5c78157
Đỗ Ngọc Quang - B19DCCN521 <doquang227@gmail.com>
2023-02-12 22:28:59 +0700

    add vercel config

--- src/module/user/user.repository.ts
+++ src/module/user/user.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/libs/database/entity/user.entity';
+import { User } from '../../libs/database/entity/user.entity';
 import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
